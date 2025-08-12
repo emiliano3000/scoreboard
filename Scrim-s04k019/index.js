35d944c908c10aa425cd72882f0d4303c302fe6b
emiliano3000 <131930075+emiliano3000@users.noreply.github.com>
2025-08-12 01:50:33 -0300

    commit initial files

---
+++ Scrim-s04k019/index.js
@@ -0,0 +1,37 @@
+
+let homeScore = document.getElementById("home-score")
+let visitorScore = document.getElementById("visitor-score")
+
+function changeScore1(){
+    console.log("I run")
+    homeScore.textContent = parseInt(homeScore.textContent) + 1 
+}
+
+function changeScore2(){
+    console.log("I run")
+    homeScore.textContent = parseInt(homeScore.textContent) + 2 
+}
+
+function changeScore3(){
+    console.log("I run")
+    homeScore.textContent = parseInt(homeScore.textContent) + 3 
+}
+
+function changeScore4(){
+    console.log("I run")
+    visitorScore.textContent = parseInt(visitorScore.textContent) + 1 
+}
+
+function changeScore5(){
+    console.log("I run")
+    visitorScore.textContent = parseInt(visitorScore.textContent) + 2 
+}
+
+function changeScore6(){
+    console.log("I run")
+    visitorScore.textContent = parseInt(visitorScore.textContent) + 3 
+}
+
+
+
+
